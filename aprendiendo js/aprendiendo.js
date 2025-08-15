a8fe665307c76b50bc828dc205d2869d2f68d3af
santiago1227 <santiago.saldana@ucb.edu.bo>
2025-08-15 10:57:53 -0400

    v2

---
+++ aprendiendo js/aprendiendo.js
@@ -0,0 +1,34 @@
+function mi_primer_algoritmo(){
+    var mensaje = "hola mundo";
+    alert (mensaje);
+}
+
+// return
+
+function binarioADecimal(binario) {
+  let decimal = 0;
+  for (let i = 0; i < binario.length; i++) {
+    digitobinario = binario[1];
+    if(digitobinario === "1"){
+        decimal = decimal * 2 + 1;
+  }
+else{
+
+      decimal = decimal * 2 + 0
+
+    }  
+    
+    alert(decimal);
+
+  }
+  function sumaprimos(){
+
+}    
+
+
+
+  
+  }
+  return decimal;
+
+
