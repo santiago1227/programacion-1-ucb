d552e55a1a91784f83cbee33d6ea09621af35439
santiago1227 <santiago.saldana@ucb.edu.bo>
2025-10-06 11:39:18 -0400

    cargar a github

--- po_clase/objeto.js
+++ po_clase/objeto.js
@@ -1,5 +1,41 @@
 class santiago{
 
-    //comment
+    //atributos de la clase
+    nombre = "";
+    edad = "";
+    genero = "";
+    tipoBoleto = "";
+
+    //constructor
+    constructor(paramNombre, paramEdad,paramGenero, paramTipoBoleto){
+        this.nombre = paramNombre;
+        this.edad = paramEdad;
+        this.genero = paramGenero;
+        this.tipoBoleto = paramTipoBoleto;
+
+    }
+
+    //setter
+    setNombre(paramNombre){this.nombre = paramNombre;}
+    setEdad(paramEdad){this.edad = paramEdad;}
+    setGenero(paramGenero){this.genero = paramGenero;}
+    setTipoBoleto(paramTipoBoleto){ this.tipoBoleto = paramTipoBoleto;}
+
+    //getter
+    getNombre(){return this.nombre;}
+    getedad(){return this.edad;}
+    getGenero(){return this.edad;}
+    getTipoBoleto(){return this.TipoBoleto;}
+
+    //mostrar los valores de los atributos
+    ToString(){
+        return "pasajeros" +
+             "nombre" + this.getNombre() +
+             "Edad" + this.getedad() +
+             "Genero" + this.getGenero() +
+             "Tipo de Boleto" + this.getTipoBoleto() +
+    }
+    
+
 
 }
